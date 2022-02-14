4638d1483d516ae188cced30953411d3ccb8dcfb
Denny Wright <dennison.wright89@gmail.com>
2022-02-14 10:58:09 -0500

    App rebuilt with node 14.15

---
+++ client/src/components/music/SheetMusic.js
@@ -0,0 +1,74 @@
+import React, { useRef, useEffect, useState } from 'react'
+import { Score } from "./VexFlowComponent"
+
+import noteTranslator from "./musicTheory/noteTranslator.js"
+import StaffDisplay from "./StaffDisplay.js"
+
+const SheetMusic = (props) => {
+const [vexNotes, setVexNotes] = useState([])
+const [clicked, setClicked] = useState(1)
+const renderCount = useRef(1)
+
+const incrementClick = () =>{
+  setClicked(clicked +1)
+}
+
+
+let score = ""
+
+const eraseNotes = () =>{
+  const element = document.getElementById("music")
+  element.remove()
+}
+
+  const generateNotes= (textNoteArray) =>{
+  }
+
+//look into minimum margins for the form div
+//put the sheet music component inside a parent div with a fixed height of ~175?
+
+
+if(props.notes.length > 0){
+  score = <Score
+  clef="treble"
+  staves={[
+    [
+      {
+        keys: props.notes[0],
+        duration: "4",
+        stem_direction: -1
+      },
+      { 
+        keys: props.notes[1],
+        duration: "4",
+        stem_direction: -1
+      },
+      { 
+        keys: props.notes[2],
+        duration: "4",
+        stem_direction: -1
+      },
+      { 
+        keys: props.notes[3],
+        duration: "4",
+        stem_direction: -1
+      }
+    ]
+  ]}
+  />
+}
+
+  useEffect(()=>{
+    setVexNotes(props.notes)
+  },[vexNotes])
+//debugger
+  return (
+    <div id="music" className="centered">
+      {score}
+      <button onClick={eraseNotes}>Click</button>
+   
+    </div>
+  )
+}
+
+export default SheetMusic
