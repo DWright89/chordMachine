d981efc9a1fbc883e77ce6715923ff1068b90e2b
Denny Wright <dennison.wright89@gmail.com>
2022-02-17 21:11:40 -0500

    still trying to fix herok

--- server/src/routes/api/v1/theoryRouter.js
+++ server/src/routes/api/v1/theoryRouter.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from 'dotenv'
-//import hookTheoryClient from "../../../services/hookTheoryClient.js"
+import hookTheoryClient from "../../../services/hookTheoryClient.js"
 
 const theoryRouter = new express.Router()
 dotenv.config()
